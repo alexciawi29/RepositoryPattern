95624acca3875e1f56b587f88ee2de400229aa17
AlexCiawi <alexciawi29@gmail.com>
2026-07-24 10:35:33 +0700

    [Updated] Server port to 3020 and refine Swagger config.

--- Main.go
+++ Main.go
@@ -8,18 +8,18 @@ import (
 	_ "tutorial/go/docs"
 )
 
-// @title           Tutorial Go API
+// @title           Go API V6
 // @version         2.0
 // @description     REST API with Gin, GORM, and Swagger — v1 uses custom query params, v2 uses OData
-// @host            localhost:8080
+// @host            localhost:3020
 // @BasePath        /
 func main() {
 	Config.ConnectDB()
 	r := Router.SetupRouter()
-	log.Println("Server berjalan di  → http://localhost:8080")
-	log.Println("Swagger UI          → http://localhost:8080/swaggerui/index.html")
-	log.Println("OpenAPI 3.0.1 JSON  → http://localhost:8080/swagger/v1/swagger.json")
-	if err := r.Run(":8080"); err != nil {
+	log.Println("Server berjalan di  → http://localhost:3020")
+	log.Println("Swagger UI          → http://localhost:3020/swaggerui/index.html")
+	log.Println("Swagger JSON        → http://localhost:3020/swaggerui/doc.json")
+	if err := r.Run(":3020"); err != nil {
 		log.Fatal("Gagal menjalankan server: ", err)
 	}
 }
